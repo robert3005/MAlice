077e8a8bbd437cf5ac7eab23bdb5de9b9ea52c15
Piotr Bar <baru02@gmail.com>
2011-11-02 16:10:00 +0000

    .

--- parser_config.js
+++ parser_config.js
@@ -10,21 +10,21 @@ createNode = (type, value, children) ->
 	n.children = child for child in arguments[2...arguments.length]
 	n
 
-NODE_OP		= 0;
-NODE_VAR	= 1;
-NODE_CONST	= 2;
+NODE_OP		= 0
+NODE_VAR	= 1
+NODE_CONST	= 2
 
-OP_NONE		= -1;
-OP_ASSIGN	= 0;
-OP_DECLARE	= 1;
-OP_OR		= 2;
-OP_XOR		= 3;
-OP_AND		= 4;
-OP_SUB		= 5;
-OP_DIV		= 6;
-OP_MOD		= 7;
-OP_UNR		= 8;
-OP_NEG		= 9;
+OP_NONE		= -1
+OP_ASSIGN	= 0
+OP_DECLARE	= 1
+OP_OR		= 2
+OP_XOR		= 3
+OP_AND		= 4
+OP_SUB		= 5
+OP_DIV		= 6
+OP_MOD		= 7
+OP_UNR		= 8
+OP_NEG		= 9
 
 *]
 
@@ -47,65 +47,65 @@ OP_NEG		= 9;
 
 ##
 
-assignment:		id function_name type separator         	[* %% = createNode( NODE_OP, OP_DECLARE, %1, %3); *]       
-			| id function_name expression			[* %% = createNode( NODE_OP, OP_ASSIGN, %1, %3 ); *]
+assignment:		id function_name type separator         	[* %% = createNode( NODE_OP, OP_DECLARE, %1, %3) *]       
+			| id function_name expression			[* %% = createNode( NODE_OP, OP_ASSIGN, %1, %3 ) *]
 			;
 
-assignment_list:	assignment_list assignment			[* %% = createNode( NODE_OP, OP_NONE, %1, %2 ); *]
+assignment_list:	assignment_list assignment			[* %% = createNode( NODE_OP, OP_NONE, %1, %2 ) *]
 			|
 			;
 
-expression:		expression 					[* %% = createNode( NODE_OP, %1 ); *]
+expression:		expression 					[* %% = createNode( NODE_OP, %1 ) *]
 			| or_expression					
 			;
 
 or_expression:		xor_expression					
-			| or_expression '|' xor_expression		[* %% = createNode( NODE_OP, OP_OR, %1, %3 ); *]
+			| or_expression '|' xor_expression		[* %% = createNode( NODE_OP, OP_OR, %1, %3 ) *]
 			;
 
 xor_expression: 	and_expression					
-			| xor_expression '^' and_expression		[* %% = createNode( NODE_OP, OP_XOR, %1, %3 ); *]
+			| xor_expression '^' and_expression		[* %% = createNode( NODE_OP, OP_XOR, %1, %3 ) *]
 			;
  
 and_expression: 	add_expression					
-			| and_expression '&' add_expression		[* %% = createNode( NODE_OP, OP_AND, %1, %3 ); *]
+			| and_expression '&' add_expression		[* %% = createNode( NODE_OP, OP_AND, %1, %3 ) *]
 			;
 
 add_expression:		mul_expression					
-			| add_expression '+' mul_expression		[* %% = createNode( NODE_OP, OP_ADD, %1, %3 ); *]
-			| add_expression '-' mul_expression		[* %% = createNode( NODE_OP, OP_SUB, %1, %3 ); *]
+			| add_expression '+' mul_expression		[* %% = createNode( NODE_OP, OP_ADD, %1, %3 ) *]
+			| add_expression '-' mul_expression		[* %% = createNode( NODE_OP, OP_SUB, %1, %3 ) *]
 			;
 
 mul_expression:		unary_expression
-			| mul_expression '/' unary_expression		[* %% = createNode( NODE_OP, OP_DIV, %1, %3 ); *]
-			| mul_expression '%' unary_expression		[* %% = createNode( NODE_OP, OP_MOD, %1, %3 ); *]
+			| mul_expression '/' unary_expression		[* %% = createNode( NODE_OP, OP_DIV, %1, %3 ) *]
+			| mul_expression '%' unary_expression		[* %% = createNode( NODE_OP, OP_MOD, %1, %3 ) *]
 			;
 
 unary_expression:	primitive_expression				
-			| un_op primitive_expression			[* %% = createNode( NODE_OP, OP_UNR, %1, %2 ); *]
+			| un_op primitive_expression			[* %% = createNode( NODE_OP, OP_UNR, %1, %2 ) *]
 			;
 
-primitive_expression:	id 						[* %% = createNode( NODE_VAR, %1 ); *]
-			| numeral 					[* %% = createNode( NODE_CONST, %1 ); *]
-			| letter 					[* %% = createNode( NODE_CONST, %1 ); *]
+primitive_expression:	id 						[* %% = createNode( NODE_VAR, %1 ) *]
+			| numeral 					[* %% = createNode( NODE_CONST, %1 ) *]
+			| letter 					[* %% = createNode( NODE_CONST, %1 ) *]
 			| '(' expression ')' 				[* %% = %2; *]
 			|
 			;
 
-type:			| 'letter' 					[* %% = createNode( NODE_CONST, %1 ); *]
-			| 'number'					[* %% = createNode( NODE_CONST, %1 ); *]
+type:			| 'letter' 					[* %% = createNode( NODE_CONST, %1 ) *]
+			| 'number'					[* %% = createNode( NODE_CONST, %1 ) *]
 			;
 
-separator:		'.' 						[* %% = createNode( NODE_OP, OP_NONE ); *]
-			| ',' 						[* %% = createNode( NODE_OP, OP_NONE ); *]
-			| 'then' 					[* %% = createNode( NODE_OP, OP_NONE ); *]
-			| 'or' 						[* %% = createNode( NODE_OP, OP_NONE ); *]
-			| 'and' 					[* %% = createNode( NODE_OP, OP_NONE ); *]
-			| 'too' 					[* %% = createNode( NODE_OP, OP_NONE ); *]
-			| 'but'						[* %% = createNode( NODE_OP, OP_NONE ); *]
+separator:		'.' 						[* %% = createNode( NODE_OP, OP_NONE ) *]
+			| ',' 						[* %% = createNode( NODE_OP, OP_NONE ) *]
+			| 'then' 					[* %% = createNode( NODE_OP, OP_NONE ) *]
+			| 'or' 						[* %% = createNode( NODE_OP, OP_NONE ) *]
+			| 'and' 					[* %% = createNode( NODE_OP, OP_NONE ) *]
+			| 'too' 					[* %% = createNode( NODE_OP, OP_NONE ) *]
+			| 'but'						[* %% = createNode( NODE_OP, OP_NONE ) *]
 			;
 
-un_op:			'~'						[* %% = createNode( NODE_OP, OP_NEG); *]
+un_op:			'~'						[* %% = createNode( NODE_OP, OP_NEG) *]
 			;
 
 [*
