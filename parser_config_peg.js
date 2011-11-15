6ce4e7e6c19b3e73708eb53b51d30f0ed961beb7
Robert Kruszewski <dzobert@gmail.com>
2011-11-15 18:56:20 +0000

    Parser IS working you lazy motherfucker. Love you. Did a little bit of housekeeping.

---
+++ parser_config_peg.js
@@ -0,0 +1,120 @@
+{
+function NODE()
+{
+	var type;
+	var value;
+	var children;
+}
+
+//Management functions
+function createNode( type, value, children )
+{
+	var n = new NODE();
+	n.type = type;
+	n.value = value;	
+	n.children = new Array();
+	
+	for( var i = 2; i < arguments.length; i++ )
+		n.children.push( arguments[i] );
+		
+	return n;
+}
+
+
+NODE_OP		= 0
+NODE_VAR	= 1
+NODE_CONST	= 2
+NODE_TYPE	= 3
+
+OP_NONE		= -1
+OP_ADD          = 1
+OP_OR		= 2
+OP_XOR		= 3
+OP_AND		= 4
+OP_SUB		= 5
+OP_MUL		= 6
+OP_DIV		= 7
+OP_MOD		= 8
+OP_UNR		= 9
+OP_NEG		= 10
+}
+
+start
+= single:assignment sep:separator list:start {return createNode( NODE_OP, OP_NONE, list, single ) }
+/ single:assignment enter:newLine list:start  {return createNode( NODE_OP, OP_NONE, list, single ) }
+/ EOF
+/ [\n] list:start {return createNode( NODE_OP, OP_NONE, list ) }
+
+assignment
+= identifier:id ' ' name:function_name ' ' type:typeName { return createNode( NODE_OP, name, identifier, type ) }
+ / identifier:id ' ' name:function_name ' ' expr:expression { return createNode( NODE_OP, name, identifier, expr ) }
+/ EOF
+
+expression
+= orExpr:or_expression
+
+or_expression
+= xorExpr:xor_expression sp:space '|' sp:space orExpr:or_expression { return createNode( NODE_OP, OP_OR, orExpr, xorExpr ) }
+/ xorExpr:xor_expression
+
+xor_expression
+= andExpr:and_expression sp:space '^' sp:space xorExpr:xor_expression { return createNode( NODE_OP, OP_XOR, xorExpr, andExpr ) }
+/ andExpr:and_expression
+
+and_expression
+= addExpr:add_expression sp:space '&' sp:space andExpr:and_expression { return createNode( NODE_OP, OP_AND, andExpr, addExpr) }
+/ addExpr:add_expression
+
+add_expression
+= mulExpr:mul_expression sp:space '+' sp:space addExpr:add_expression { return createNode(NODE_OP, OP_ADD, addExpr, mulExpr) }
+/ mulExpr:mul_expression sp:space '-' sp:space addExpr:add_expression { return createNode(NODE_OP, OP_SUB, addExpr, mulExpr) }
+/ mulExpr:mul_expression
+
+mul_expression
+= unExpr:unary_expression sp:space '*'  sp:space mulExpr:mul_expression  { return createNode(NODE_OP, OP_MUL, mulExpr, unExpr) }
+/ unExpr:unary_expression sp:space '/' sp:space mulExpr:mul_expression { return createNode(NODE_OP, OP_DIV, mulExpr, unExpr) }
+/ unExpr:unary_expression sp:space '%' sp:space mulExpr:mul_expression  { return createNode(NODE_OP, OP_MOD, mulExpr, unExpr) }
+/ unExpr:unary_expression
+
+unary_expression
+= primExpr:primitive_expression
+/ unOP:un_op primEpxr:primitive_expression { return createNode(NODE_OP, OP_UNR, unOP, primExpr ) }
+
+primitive_expression
+= num:[0-9]+ { return createNode( NODE_CONST, num.join("") ) }
+/ letter:([\'][A-Za-z][\']) { return createNode( NODE_CONST, letter ) }
+/ identifier:id
+
+typeName
+= type:'letter' { return createNode( NODE_TYPE, type) }
+/ type:'number' { return createNode( NODE_TYPE, type) }
+
+id
+= identifier:[A-Za-z][A-Za-z_]* { return identifier }
+
+function_name
+= funcName:'was a' { return funcName }
+/ funcName:'became' { return funcName }
+
+newLine
+= sep:([\.] EOF) { return createNode( NODE_OP, OP_NONE) }
+/ sep:([\.][\n]) { return createNode( NODE_OP, OP_NONE) }
+/ sep:([\,][\n]) { return createNode( NODE_OP, OP_NONE) }
+
+space
+= sp:' '
+/ sp:''
+
+separator
+= sep:([\,][ ]) { return createNode( NODE_OP, OP_NONE) }
+/ sep:([ ]'then'[ ]) { return createNode( NODE_OP, OP_NONE) }
+/ sep:([ ]'or'[ ]) { return createNode( NODE_OP, OP_NONE) }
+/ sep:([ ]'and'[ ])  { return createNode( NODE_OP, OP_NONE) }
+/ sep:([ ]'too'[ ])  { return createNode( NODE_OP, OP_NONE) }
+/ sep:([ ]'but'[ ])  { return createNode( NODE_OP, OP_NONE) }
+
+un_op
+= unOP:'~' { return createNode( NODE_OP, OP_NEG)  }
+
+EOF
+  = !.
