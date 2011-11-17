114ba70269ea179d4515cc6cba030030a7bed9a1
Lukasz Koprowski <azram19@azram19-dold.(none)>
2011-11-17 03:30:25 +0000

    Almost builds. Too tired to dig any further, sth is wrong with parameters for a compiler.

--- code_ast.hpp
+++ code_ast.hpp
@@ -1,47 +1,88 @@
-#include <String>
+#ifndef AST_H_GUARD
+#define AST_H_GUARD
 
-enum NodeType {OP = 0, VAR, CONST, TYPE}
-enum OPType {NONE = 0, ADD, OR, XOR, AND, SUB, MUL, DIV, MOD, UNR, NEG}
-enum VarType {STRING = 0, NUMBER}
+#include <string>
+#include <vector>
+#include <map>
+#include <utility>
+#include <list>
 
-typedef Value* (*OPGenFunction)( Module*, Builder* );
-typedef Node* (*NodeGenFunction)( NodeType, OPType, const char* Format, ... );
+#include "llvm/LLVMContext.h"
+#include "llvm/Module.h"
+#include "llvm/DerivedTypes.h"
+#include "llvm/Constants.h"
+#include "llvm/Instructions.h"
+#include "llvm/Analysis/Verifier.h"
+#include "llvm/ExecutionEngine/JIT.h"
+#include "llvm/ExecutionEngine/Interpreter.h"
+#include "llvm/ExecutionEngine/GenericValue.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Target/TargetSelect.h"
+
+enum NodeType {OP = 0, VAR, CONST, TYPE};
+enum OPType {NONE = 0, ADD, OR, XOR, AND, SUB, MUL, DIV, MOD, UNR, NEG};
+enum VarType {STRING = 0, NUMBER};
+/*
+typedef Value* Valueptr
+typedef Value * (*OPGenFunction)( *Module, *Builder );
 
 class SymbolTable{
 	
 };
-
+*/
 class SimpleNode{
+	public:
+		SimpleNode( std::string, std::string, std::string, std::string );
+
+		NodeType getType();
+		int getId();
+		std::string getData();
+
 	protected:
+		int uniqueId; 
+
 		//general
 		NodeType type;
 		OPType op;
-		std::String id;
-		VarType varType;
 		
-		//values
-		int valueInt;
-		std::String valueString;
+		//data left to parse
+		std::string data;
 
-		//children
-		SimpleNode children;
 
-	public:
-		SimpleNode();
 };
 
 class Node : public SimpleNode{
 	public:
-		virtual Value *codeGen( Module*, Builder* ) = 0;
-		static Node createAST( SimpleNode ); //Factory
+		virtual Value *codeGen( Module&, Builder& ) = 0;
+		void addChild( Node* );
+
+		static Node createAST( std::map<int, SimpleNode>& ); //Factory
 	
 	protected:
-		NodeGenFunction codeGenFunctions[11] = { NULL };
+		Node( SimpleNode );
+		Value * (*OPGenFunction)( *Module, *Builder ) codeGenFunctions[11] = { NULL };
+
+		static Node * createOPNode( const SimpleNode&, std::list<std::pair<int, int> >& );
+		static Node * createVARNode( const SimpleNode&, std::list<std::pair<int, int> >& );
+		static Node * createCONSTNode( const SimpleNode&, std::list<std::pair<int, int> >& );
+		static Node * createTYPENode( const SimpleNode&, std::list<std::pair<int, int> >& );
+
+		//TODO: decide which fields to leave here, and which to move down
+		std::string id;
+		VarType varType;
+		
+		//values
+		int valueInt;
+		std::string valueString;
+
+		//children
+		std::vector<SimpleNode> children;
+
 };
 
 class OPNode : public Node{
 	public:
-		Value *codeGen( Module*, Builder* );
+		Value *codeGen( Module&, Builder& );
 
 	protected:
 
@@ -61,20 +102,22 @@ class OPNode : public Node{
 
 class VARNode : public Node{
 		public:
-		Value *codeGen( Module*, Builder* );
+		Value *codeGen( Module&, Builder& );
 };
 
 class CONSTNode : public Node{
 	public:
-		Value *codeGen( Module*, Builder* );	
+		Value *codeGen( Module&, Builder& );	
 
 	protected:
 
-		Value *codeGenSTRING( Module*, Builder* );
-		Value *codeGenNUMBER( Module*, Builder* );
-}
+		Value *codeGenSTRING( Module&, Builder& );
+		Value *codeGenNUMBER( Module&, Builder& );
+};
 
 class TYPENode : public Node{
 	public:
-		Value *codeGen( Module*, Builder* );	
-}
+		Value *codeGen( Module&, Builder& );	
+};
+
+#endif
--- code_generator.cpp
+++ code_generator.cpp
@@ -1,20 +1,69 @@
-#include <String>
+#include <string>
+#include <map>
+#include <utility>
+#include <stack>
 
+#include "llvm/LLVMContext.h"
 #include "llvm/Module.h"
-#include "llvm/Function.h"
-#include "llvm/PassManager.h"
-#include "llvm/CallingConv.h"
+#include "llvm/DerivedTypes.h"
+#include "llvm/Constants.h"
+#include "llvm/Instructions.h"
 #include "llvm/Analysis/Verifier.h"
-#include "llvm/Assembly/PrintModulePass.h"
-#include "llvm/Support/IRBuilder.h"
+#include "llvm/ExecutionEngine/JIT.h"
+#include "llvm/ExecutionEngine/Interpreter.h"
+#include "llvm/ExecutionEngine/GenericValue.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Target/TargetSelect.h"
+
 
 #include "code_generator.hpp"
-
-#include "code_ast.cpp"
+#include "code_ast.hpp"
 
 using namespace llvm;
 
+/**
+ * Converts string representation on a node to SimpleNode object
+ */
+/*std::map<int, SimpleNode* > CodeGenerator::parse( rawData ){
+	std::map<int, SimpleNode> mapOfNodes;
+
+	std::size_t pos;
+	int last = 0;
+
+	std::string node;
+	std::string nodeIdNumber;
+	std::string nodeType;
+	std::string opType;
+	std::string args;
+
+	std::size_t fieldPos;
+	int lastFieldPos = 0;
+
+	while( std::string::npos != ( pos = rawData.find( "#" ) ) ){
+		node = rawData.substr( last + 1, ( int( pos ) - last - 1 ) );
+
+		lastFieldPos = 0
+		fieldPos = node.find( "|" );
+		nodeIdNumber = node.substr( 0, fieldPos );
+
+		lastFieldPos = int( fieldPos );
+		fieldPos = node.find( "|" );
+		nodeType = node.substr( lastFieldPos + 1, ( int( fieldPos ) - lastFieldPos - 1 ) );
+
+		lastFieldPos = int( fieldPos );
+		fieldPos = node.find( "|" );
+		opType = node.substr( lastFieldPos + 1, ( int( fieldPos ) - lastFieldPos - 1 ) );
+
+		lastFieldPos = int( fieldPos );
+		fieldPos = node.find( "|" );
+		args = node.substr( lastFieldPos + 1, ( int( fieldPos ) - lastFieldPos - 1 ) );
+
+		mapOfNodes[atoi( nodeIdNumber.c_str() )] = new SimpleNode( nodeIdNumber, nodeType, opType, args );
+
+		last = int(pos);
+	}
+}*/
+/*
 Module* makeLLVMModule( Node ast ){
 	Module* mod = new Module( "alice", getGlobalContext() );
 
@@ -25,8 +74,8 @@ Module* makeLLVMModule( Node ast ){
 
 int main(){
 	CodeGenerator = new CodeGenerator();
-	std::String rawDataFromParser;
-	SimpleNode dataFromParser;
+	std::string rawDataFromParser = "0#OP#ADD#1,2#1,2|1#CONST#NONE##1|2#CONST#NONE##2";
+	std::map<int, SimpleNode> dataFromParser;
 
 	//read data input
 
@@ -44,4 +93,14 @@ int main(){
 	delete Mod;
 	return 0;
 
+}*/
+
+int main(){
+	std::string rawDataFromParser = "0#OP#ADD#1,2#1,2|1#CONST#NONE##1|2#CONST#NONE##2";
+	std::map<int, SimpleNode> dataFromParser;
+
+	//read data input
+
+	//dataFromParser = CodeGenerator.parse( rawDataFromParser );
+	//Node ast = Node::generateAST( dataFromParser );
 }
