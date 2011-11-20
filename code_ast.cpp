4d4531af90d37163abb30e93df35a71e11a2d308
azram19 <azram19@gmail.com>
2011-11-20 16:04:38 +0000

    ...

--- code_ast.cpp
+++ code_ast.cpp
@@ -247,7 +247,7 @@ Value* CONSTNode::codeGenSTRING( CONSTNode& n ){
 }
 
 Value* CONSTNode::codeGenNUMBER( CONSTNode& n ){
-	printf("CONSTNode::codeGenNUMBER CG\n");
+	printf("CONSTNode::codeGenNUMBER CG %d\n", n.getValueNumber());
 	return ConstantInt::get( Type::getInt32Ty( getGlobalContext() ), n.getValueNumber() );
 }
 
