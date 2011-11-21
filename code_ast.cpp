37309d6eb67eb7b89f976c27493e545a10587298
azram19 <azram19@gmail.com>
2011-11-21 11:30:12 +0000

    ...

--- code_ast.cpp
+++ code_ast.cpp
@@ -351,9 +351,9 @@ Value * VARNode::codeGen(){
 
 	Function *TheFunction = Builder.GetInsertBlock() -> getParent();
 	IRBuilder<> TmpB( &TheFunction -> getEntryBlock(), TheFunction -> getEntryBlock().begin() );
-	
-	AllocaInst * Alloca = TmpB.CreateAlloca(Type::getInt32Ty(getGlobalContext()), 0);
 	/*
+	AllocaInst * Alloca = TmpB.CreateAlloca(Type::getInt32Ty(getGlobalContext()), 0);
+	
 	Value * S = Builder.CreateStore(lhs, Alloca);
 
 	S -> dump();
