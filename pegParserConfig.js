a4e3e889ca329e08abf3f887ca0d4f33e168fd96
Robert Kruszewski <dzobert@gmail.com>
2011-12-06 14:19:56 +0000

    obvious improvement to node indexing

--- pegParserConfig.js
+++ pegParserConfig.js
@@ -14,7 +14,7 @@
 	//Management functions
 	function createNode( type, value, children ) {
 		var n = new NODE();
-		n.id = counter;
+		n.id = counter++;
 		n.type = type;
 		n.value = value;	
 		n.children = new Array();
@@ -131,46 +131,46 @@ instructions
 / assignment_line
 
 if_block
-= space name:ifFun space cond:condition space 'so' space [\n]* space ifB:start ifE:ifelse {counter++; return createNode(NODE_IF, cond, ifB, ifE)}
+= space name:ifFun space cond:condition space 'so' space [\n]* space ifB:start ifE:ifelse { return createNode(NODE_IF, cond, ifB, ifE)}
 
 ifelse
-= space 'or' space [\n]* ifBody:function_body ife:ifelse {counter++; return createNode(NODE_ELSE, "else", ifBody, ife)}
-/ space 'or maybe' space cond:condition space 'so' space [\n]* ifBody:function_body ife:ifelse {counter++; return createNode(NODE_ELSE_IF, cond, ifBody, ife)}
-/ space unsure newLine {counter++; return createNode( NODE_END_IF, "endif" ) }
+= space 'or' space [\n]* ifBody:function_body ife:ifelse { return createNode(NODE_ELSE, "else", ifBody, ife)}
+/ space 'or maybe' space cond:condition space 'so' space [\n]* ifBody:function_body ife:ifelse { return createNode(NODE_ELSE_IF, cond, ifBody, ife)}
+/ space unsure newLine { return createNode( NODE_END_IF, "endif" ) }
 
 func_call
 = fun:function newLine { return fun; }
 / fun:function separator { return fun; }
 
 function
-= space identifier:id space '(' arglist:argument_fun* ')' space {counter++; return createNode(NODE_FUN_CALL, identifier, arglist)}
-/ space arg:id space 'went through' space identifier:id {counter++; return createNode(NODE_FUN_CALL, arg, identifier)}
+= space identifier:id space '(' arglist:argument_fun* ')' space { return createNode(NODE_FUN_CALL, identifier, arglist)}
+/ space arg:id space 'went through' space identifier:id { return createNode(NODE_FUN_CALL, arg, identifier)}
 
 argument_fun
 = space arg:legalArgs separator? space { return arg }
 
 func_block
-= space 'The room' space def:function_type 'contained a' space type:typeName [\n]* funcB:function_body {counter++; return createNode( NODE_FUN_DEF, type, def, funcB) }
-/ space 'The Looking-Glass' space identifier:id space 'changed a' space type:typeName [\n]* funcB:function_body {counter++; return createNode( NODE_LOOK_DEF, identifier, type, funcB)}
+= space 'The room' space def:function_type 'contained a' space type:typeName [\n]* funcB:function_body { return createNode( NODE_FUN_DEF, type, def, funcB) }
+/ space 'The Looking-Glass' space identifier:id space 'changed a' space type:typeName [\n]* funcB:function_body { return createNode( NODE_LOOK_DEF, identifier, type, funcB)}
 
 function_type
-= identifier:id space '(' arglist:argument_type* ')' space {counter++; return createNode(NODE_FUN, identifier, arglist)}
+= identifier:id space '(' arglist:argument_type* ')' space { return createNode(NODE_FUN, identifier, arglist)}
 
 argument_type
-= space stype:spiderType? space type:typeName space identifier:id separator? space { counter++; return createNode( NODE_TYPE, "argument", identifier, type, stype ) }
+= space stype:spiderType? space type:typeName space identifier:id separator? space {  return createNode( NODE_TYPE, "argument", identifier, type, stype ) }
 
 loop_block
-= space 'eventually' space cond:condition space 'because' space [\n]* loop:function_body space [\n]* space 'enough' space 'times' newLine {counter++; return createNode( NODE_LOOP, cond, loop )}
+= space 'eventually' space cond:condition space 'because' space [\n]* loop:function_body space [\n]* space 'enough' space 'times' newLine { return createNode( NODE_LOOP, cond, loop )}
 
 io_line
 = single:io separator { return single }
 / single:io newLine { return single }
 
 io
-= space arg:legalArgs space name:function_output space 'Alice' {counter++; return createNode( NODE_IO, name, arg)}
-/ space arg:legalArgs space name:function_output {counter++; return createNode(NODE_IO, name, arg)}
-/ space 'Alice' space 'found' space arg:legalArgs space {counter++; return createNode(NODE_IO, 'found', arg)}
-/ space name:function_input space arg:argument {counter++; return createNode(NODE_IO, name, arg)} 
+= space arg:legalArgs space name:function_output space 'Alice' { return createNode( NODE_IO, name, arg)}
+/ space arg:legalArgs space name:function_output { return createNode(NODE_IO, name, arg)}
+/ space 'Alice' space 'found' space arg:legalArgs space { return createNode(NODE_IO, 'found', arg)}
+/ space name:function_input space arg:argument { return createNode(NODE_IO, name, arg)} 
 
 legalArgs
 = fun:function {return fun}
@@ -183,68 +183,68 @@ assignment_line
 / single:assignment newLine { return single }
 
 assignment
-= space identifier:id space name:function_name space type:typeName { counter++; return createNode( NODE_TYPE, name, identifier, type ) }
- / space arg:argument space name:function_name space expr:legalArgs { counter++; return createNode( NODE_VAR, name, arg, expr ) }
- / space identifier:id space name:function_name { counter++; return createNode( NODE_VAR, name, identifier ) }
-/ space identifier:id space 'had' space expr:expression space type:typeName { counter++; return createNode( NODE_TYPE, 'array', identifier, expr, type ) }
+= space identifier:id space name:function_name space type:typeName {  return createNode( NODE_TYPE, name, identifier, type ) }
+ / space arg:argument space name:function_name space expr:legalArgs {  return createNode( NODE_VAR, name, arg, expr ) }
+ / space identifier:id space name:function_name {  return createNode( NODE_VAR, name, identifier ) }
+/ space identifier:id space 'had' space expr:expression space type:typeName {  return createNode( NODE_TYPE, 'array', identifier, expr, type ) }
 
 expression
 = logical_or_exp
 
 logical_or_exp
-=  laExpr:logical_and_exp space '||' space loExpr:logical_or_exp { counter++; return createNode(NODE_OP, OP_LOR, laExpr, loExpr) }
+=  laExpr:logical_and_exp space '||' space loExpr:logical_or_exp {  return createNode(NODE_OP, OP_LOR, laExpr, loExpr) }
 / logical_and_exp
 
 logical_and_exp	
-= orExpr:or_expression space '&&' space laExpr:logical_and_exp { counter++; return createNode(NODE_OP, OP_LAND, orExpr, laExpr) }
+= orExpr:or_expression space '&&' space laExpr:logical_and_exp {  return createNode(NODE_OP, OP_LAND, orExpr, laExpr) }
 / or_expression
 
 or_expression
-= xorExpr:xor_expression space '|' space orExpr:or_expression { counter++; return createNode( NODE_OP, OP_OR, xorExpr, orExpr ) }
+= xorExpr:xor_expression space '|' space orExpr:or_expression {  return createNode( NODE_OP, OP_OR, xorExpr, orExpr ) }
 / xor_expression
 
 xor_expression
-= andExpr:and_expression space '^' space xorExpr:xor_expression { counter++; return createNode( NODE_OP, OP_XOR, andExpr, xorExpr ) }
+= andExpr:and_expression space '^' space xorExpr:xor_expression {  return createNode( NODE_OP, OP_XOR, andExpr, xorExpr ) }
 / and_expression
 
 and_expression
-= eqExpr:equality_exp space '&' space andExpr:and_expression { counter++; return createNode( NODE_OP, OP_AND, eqExpr, andExpr) }
+= eqExpr:equality_exp space '&' space andExpr:and_expression {  return createNode( NODE_OP, OP_AND, eqExpr, andExpr) }
 / equality_exp
 
 equality_exp
-= relExpr:relational_exp space '==' space eqExpr:equality_exp { counter++; return createNode(NODE_OP, OP_EQ, relExpr, eqExpr) }
-/ relExpr:relational_exp space '!=' space eqExpr:equality_exp { counter++; return createNode(NODE_OP, OP_UEQ, relExpr, eqExpr) }
+= relExpr:relational_exp space '==' space eqExpr:equality_exp {  return createNode(NODE_OP, OP_EQ, relExpr, eqExpr) }
+/ relExpr:relational_exp space '!=' space eqExpr:equality_exp {  return createNode(NODE_OP, OP_UEQ, relExpr, eqExpr) }
 / relational_exp
 
 relational_exp
-= addExpr:add_expression space '<' space relExpr:relational_exp { counter++; return createNode(NODE_OP, OP_LT, addExpr, relExpr) }
-/ addExpr:add_expression space '>' space relExpr:relational_exp { counter++; return createNode(NODE_OP, OP_GT, addExpr, relExpr) }
-/ addExpr:add_expression space '<=' space relExpr:relational_exp { counter++; return createNode(NODE_OP, OP_LTE, addExpr, relExpr) }
-/ addExpr:add_expression space '>=' space relExpr:relational_exp  { counter++; return createNode(NODE_OP, OP_GTE, addExpr, relExpr) }
+= addExpr:add_expression space '<' space relExpr:relational_exp {  return createNode(NODE_OP, OP_LT, addExpr, relExpr) }
+/ addExpr:add_expression space '>' space relExpr:relational_exp {  return createNode(NODE_OP, OP_GT, addExpr, relExpr) }
+/ addExpr:add_expression space '<=' space relExpr:relational_exp {  return createNode(NODE_OP, OP_LTE, addExpr, relExpr) }
+/ addExpr:add_expression space '>=' space relExpr:relational_exp  {  return createNode(NODE_OP, OP_GTE, addExpr, relExpr) }
 / add_expression
 
 add_expression
-= notExpr:not_expression space '+' space addExpr:add_expression { counter++; return createNode(NODE_OP, OP_ADD, notExpr, addExpr) }
-/ notExpr:not_expression space '-' space addExpr:add_expression { counter++; return createNode(NODE_OP, OP_SUB, notExpr, addExpr ) }
+= notExpr:not_expression space '+' space addExpr:add_expression {  return createNode(NODE_OP, OP_ADD, notExpr, addExpr) }
+/ notExpr:not_expression space '-' space addExpr:add_expression {  return createNode(NODE_OP, OP_SUB, notExpr, addExpr ) }
 / not_expression
 
 not_expression
-= notExpr:not_op space mulExpr:mul_expression {counter++; return createNode(NODE_OP, notExpr, mulExpr)}
+= notExpr:not_op space mulExpr:mul_expression { return createNode(NODE_OP, notExpr, mulExpr)}
 / mul_expression
 
 mul_expression
-= unExpr:unary_expression space '*'  space mulExpr:mul_expression  { counter++; return createNode(NODE_OP, OP_MUL, unExpr, mulExpr) }
-/ unExpr:unary_expression space '/' space mulExpr:mul_expression { counter++; return createNode(NODE_OP, OP_DIV, unExpr, mulExpr) }
-/ unExpr:unary_expression space '%' space mulExpr:mul_expression  { counter++; return createNode(NODE_OP, OP_MOD, unExpr, mulExpr) }
+= unExpr:unary_expression space '*'  space mulExpr:mul_expression  {  return createNode(NODE_OP, OP_MUL, unExpr, mulExpr) }
+/ unExpr:unary_expression space '/' space mulExpr:mul_expression {  return createNode(NODE_OP, OP_DIV, unExpr, mulExpr) }
+/ unExpr:unary_expression space '%' space mulExpr:mul_expression  {  return createNode(NODE_OP, OP_MOD, unExpr, mulExpr) }
 / unary_expression
 
 unary_expression
-= unOP:neg_op primExpr:primitive_expression { counter++; return createNode(NODE_OP, unOP, primExpr ) }
+= unOP:neg_op primExpr:primitive_expression {  return createNode(NODE_OP, unOP, primExpr ) }
 / primExpr:primitive_expression
 
 primitive_expression
-= num:[0-9]+ { counter++; return createNode( NODE_CONST, num.join(""), 'number' ) }
-/ letter:([\'][^\'][\']) {counter++; return createNode( NODE_CONST, letter[1], 'letter' ) }
+= num:[0-9]+ {  return createNode( NODE_CONST, num.join(""), 'number' ) }
+/ letter:([\'][^\'][\']) { return createNode( NODE_CONST, letter[1], 'letter' ) }
 / fun:function {return fun}
 / arg:argument {return arg}
 / str:string {return str}
