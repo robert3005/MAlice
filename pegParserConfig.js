b1fad346f8cb134727d48c749c903da1b769f40d
Robert Kruszewski <dzobert@gmail.com>
2011-12-13 04:14:19 +0000

    modified parserConfig to use external types, fixed indentation and some return types from parsing

--- pegParserConfig.js
+++ pegParserConfig.js
@@ -21,29 +21,29 @@ children.
 		var children;
 	};
 
-      function computePosition(position) {
-        var line = 1;
-        var column = 1;
-        var seenCR = false;
-        
-        for (var i = 0; i < position; i++) {
-          var ch = input.charAt(i);
-          if (ch === '\n') {
-            if (!seenCR) { line++; }
-            column = 1;
-            seenCR = false;
-          } else if (ch === '\r' | ch === '\u2028' || ch === '\u2029') {
-            line++;
-            column = 1;
-            seenCR = true;
-          } else {
-            column++;
-            seenCR = false;
-          }
-        }
-        
-        return { line: line, column: column };
-      };
+	function computePosition(position) {
+		var line = 1;
+		var column = 1;
+		var seenCR = false;
+		
+		for (var i = 0; i < position; i++) {
+			var ch = input.charAt(i);
+			if (ch === '\n') {
+				if (!seenCR) { line++; }
+				column = 1;
+				seenCR = false;
+			} else if (ch === '\r' | ch === '\u2028' || ch === '\u2029') {
+				line++;
+				column = 1;
+				seenCR = true;
+			} else {
+				column++;
+				seenCR = false;
+			}
+		}
+		
+		return { line: line, column: column };
+	};
 
 	//Management functions
 	function createNode( tagKey, type, value, children ) {
@@ -67,50 +67,16 @@ children.
 		n.numberOfChildren = n.children.length;
 		return n;
 	};
-/*
-Below there is a list of node types and a list of operator types. Every time
-a node is created, a type has to be specified. The same process concerns
-the operations, every operation has a different type.
-*/
-	NODE_ROOT		= "ROOT"
-	NODE_OP			= "OPERATOR"
-	NODE_VAR 		= "VARIABLE"
-	NODE_CONST		= "CONSTANT"
-	NODE_TYPE		= "TYPE"
-	NODE_RETURN		= "RETURN"
-	NODE_IO			= "IO"
-	NODE_LOOP 		= "WHILE"
-	NODE_FUN 		= "FUNCTION"
-	NODE_FUN_DEF	= "FUNCTION_DEFINITION"
-	NODE_IF			= "IF"
-	NODE_ELSE		= "ELSE"
-	NODE_ELSE_IF	= "ELSE_IF"
-	NODE_FUN_CALL	= "FUNCTION_CALL"
-	NODE_LOOK_DEF	= "LOOKING_GLASS"
-	NODE_END_IF		= "END_IF"
-	NODE_VAR_ARRAY	= "ARRAY"
-	NODE_STRING		= "STRING"
 
-	OP_NONE		= "NO_OP"
-	OP_ADD		= "ADD"
-	OP_OR		= "OR"
-	OP_XOR		= "XOR"
-	OP_AND		= "AND"
-	OP_SUB		= "SUBTRACT"
-	OP_MUL		= "MULTIPLY"
-	OP_DIV		= "DIVIDE"
-	OP_MOD		= "MOD"
-	OP_NOT		= "NOT"
-	OP_NEG		= "NEGATE"
-	OP_LOR		= "LOGICAL_OR"
-	OP_LAND		= "LOGICAL_AND"
-	OP_EQ		= "EQUAL"
-	OP_UEQ		= "NOT_EQUAL"
-	OP_LT		= "LESS_THAN"
-	OP_GT		= "GREATER_THAN"
-	OP_LTE		= "LESS_THAN_EQUAL"
-	OP_GTE		= "GREATER_THAN_EQUAL"
+	//console.log(process.argv)
 
+	/*
+	Node types and a list of operator types are defined for whole compiler in constants.coffee.
+	Every time a node is created, a type has to be specified. The same process concerns
+	the operations, every operation has a different type.
+	*/
+
+	var Types = require(process.cwd() + '/constants.coffee');
 }
 
 /*
@@ -121,7 +87,7 @@ in the rules in the particular order, because they are matched from top to botto
 */
 
 start
-= program:root* { return createNode( cacheKey, NODE_ROOT, "root", program);}
+= program:root* { return createNode( cacheKey, Types.NODE_ROOT, "root", program);}
 
 root
 = loop_block
@@ -152,12 +118,12 @@ else case or a line which signalises the end of an if block.
 */
 
 if_block
-= space name:ifFun space cond:condition space 'so' space [\n]* space ifB:function_body ifE:ifelse { return createNode( cacheKey, NODE_IF, "if", cond, ifB, ifE)}
+= space name:ifFun space cond:condition space 'so' space [\n]* space ifB:function_body ifE:ifelse { return createNode( cacheKey, Types.NODE_IF, "if", cond, ifB, ifE)}
 
 ifelse
-= space 'or' space [\n]* ifBody:function_body ife:ifelse { return createNode( cacheKey, NODE_ELSE, "else", ifBody, ife)}
-/ space 'or maybe' space cond:condition space 'so' space [\n]* ifBody:function_body ife:ifelse { return createNode( cacheKey, NODE_ELSE_IF, "else if", cond, ifBody, ife)}
-/ space unsure newLine { return createNode( cacheKey, NODE_END_IF, "endif" ) }
+= space 'or' space [\n]* ifBody:function_body ife:ifelse { return createNode( cacheKey, Types.NODE_ELSE, "else", ifBody, ife)}
+/ space 'or maybe' space cond:condition space 'so' space [\n]* ifBody:function_body ife:ifelse { return createNode( cacheKey, Types.NODE_ELSE_IF, "else if", cond, ifBody, ife)}
+/ space unsure newLine { return createNode( cacheKey, Types.NODE_END_IF, "endif" ) }
 
 /*
 Function call finds a pattern of a type function(type var_name, type var_name ...).
@@ -170,8 +136,8 @@ func_call
 / fun:function separator { return fun; }
 
 function
-= space identifier:id space '(' arglist:argument_fun* ')' space { return createNode( cacheKey, NODE_FUN_CALL, "function_call", identifier, arglist)}
-/ space arg:id space 'went through' space identifier:id { return createNode( cacheKey, NODE_FUN_CALL, "function_call_by_reference", arg, identifier)}
+= space identifier:id space '(' arglist:argument_fun* ')' space { return createNode( cacheKey, Types.NODE_FUN_CALL, "function_call", identifier, arglist)}
+/ space arg:id space 'went through' space identifier:id { return createNode( cacheKey, Types.NODE_FUN_CALL, "function_call_by_reference", arg, identifier)}
 
 argument_fun
 = space arg:legalArgs separator? space { return arg }
@@ -184,21 +150,21 @@ all the types of function's arguments.
 */
 
 func_block
-= space 'The room' space def:function_type 'contained a' space type:typeName [\n]* funcB:function_body { return createNode( cacheKey, NODE_FUN_DEF, "function_definition", type, def, funcB) }
-/ space 'The Looking-Glass' space identifier:id space 'changed a' space type:typeName [\n]* funcB:function_body { return createNode( cacheKey, NODE_LOOK_DEF, identifier, type, funcB)}
+= space 'The room' space def:function_type 'contained a' space type:typeName [\n]* funcB:function_body { return createNode( cacheKey, Types.NODE_FUN_DEF, "function_definition", type, def, funcB) }
+/ space 'The Looking-Glass' space identifier:id space 'changed a' space type:typeName [\n]* funcB:function_body { return createNode( cacheKey, Types.NODE_LOOK_DEF, identifier, type, funcB)}
 
 function_type
-= identifier:id space '(' arglist:argument_type* ')' space { return createNode( cacheKey, NODE_FUN, "function", identifier, arglist)}
+= identifier:id space '(' arglist:argument_type* ')' space { return createNode( cacheKey, Types.NODE_FUN, "function", identifier, arglist)}
 
 argument_type
-= space stype:spiderType? space type:typeName space identifier:id separator? space {  return createNode( cacheKey, NODE_TYPE, "argument", identifier, type, stype ) }
+= space stype:spiderType? space type:typeName space identifier:id separator? space {  return createNode( cacheKey, Types.NODE_TYPE, "argument", identifier, type, stype ) }
 
 /*
 Loop block contains anything besides a function definition.
 */
 
 loop_block
-= space 'eventually' space cond:condition space 'because' space [\n]* loop:function_body space [\n]* space 'enough' space 'times' newLine { return createNode( cacheKey, NODE_LOOP, "while", createNode( cackeKey, NODE_OP, OP_NOT, cond), loop )}
+= space 'eventually' space cond:condition space 'because' space [\n]* loop:function_body space [\n]* space 'enough' space 'times' newLine { return createNode( cacheKey, Types.NODE_LOOP, "while", createNode( cacheKey, Types.NODE_OP, Types.OP_NOT, cond), loop )}
 
 /*
 Io block matches all the input/output statements and creates appropriate nodes.
@@ -212,10 +178,10 @@ io_line
 / single:io newLine { return single }
 
 io
-= space arg:legalArgs space name:function_output { return createNode( cacheKey, NODE_IO, name, arg)}
-/ space 'Alice' space 'found' space arg:legalArgs space { return createNode( cacheKey, NODE_IO, 'found', arg)}
-/ space name:function_input space arg:argument { return createNode( cacheKey, NODE_IO, name, arg)} 
-/ space string space name:comment_function space 'Alice' { return createNode( cacheKey, NODE_IO, name, arg) }
+= space arg:legalArgs space name:function_output { return createNode( cacheKey, Types.NODE_IO, name, arg)}
+/ space 'Alice' space 'found' space arg:legalArgs space { return createNode( cacheKey, Types.NODE_IO, 'found', arg)}
+/ space name:function_input space arg:argument { return createNode( cacheKey, Types.NODE_IO, name, arg)} 
+/ space string space name:comment_function space 'Alice' { return createNode( cacheKey, Types.NODE_IO, name, arg) }
 
 /*
 All the legal arguments. They are used in ouput operations, assignments and function calls.
@@ -236,10 +202,10 @@ assignment_line
 / single:assignment newLine { return single }
 
 assignment
-= space arg:id space name:type_operator space type:typeName {  return createNode( cacheKey, NODE_FUN_CALL, name, arg, type ) }
-/ space arg:argument space name:binary_operators space expr:legalArgs {  return createNode( cacheKey, NODE_FUN_CALL, name, arg, expr ) }
-/ space identifier:id space name:unary_operators {  return createNode( cacheKey, NODE_FUN_CALL, name, identifier ) }
-/ space identifier:id space 'had' space expr:expression space type:typeName {  return createNode( cacheKey, NODE_VAR_ARRAY, "array", identifier, expr, type ) }
+= space arg:id space name:type_operator space type:typeName {  return createNode( cacheKey, Types.NODE_FUN_CALL, name, arg, type ) }
+/ space arg:argument space name:binary_operators space expr:legalArgs {  return createNode( cacheKey, Types.NODE_FUN_CALL, name, arg, expr ) }
+/ space identifier:id space name:unary_operators {  return createNode( cacheKey, Types.NODE_FUN_CALL, name, identifier ) }
+/ space identifier:id space 'had' space expr:expression space type:typeName {  return createNode( cacheKey, Types.NODE_FUN_CALL, 'had', identifier, expr, type ) }
 
 /*
 All the possible expressions are covered below. They are sorted in order
@@ -250,59 +216,59 @@ expression
 = logical_or_exp
 
 logical_or_exp
-=  laExpr:logical_and_exp space '||' space loExpr:logical_or_exp {  return createNode( cacheKey, NODE_OP, OP_LOR, laExpr, loExpr) }
+=  laExpr:logical_and_exp space '||' space loExpr:logical_or_exp {  return createNode( cacheKey, Types.NODE_OP, Types.OP_LOR, laExpr, loExpr) }
 / logical_and_exp
 
 logical_and_exp	
-= orExpr:or_expression space '&&' space laExpr:logical_and_exp {  return createNode( cacheKey, NODE_OP, OP_LAND, orExpr, laExpr) }
+= orExpr:or_expression space '&&' space laExpr:logical_and_exp {  return createNode( cacheKey, Types.NODE_OP, Types.OP_LAND, orExpr, laExpr) }
 / or_expression
 
 or_expression
-= xorExpr:xor_expression space '|' space orExpr:or_expression {  return createNode( cacheKey, NODE_OP, OP_OR, xorExpr, orExpr ) }
+= xorExpr:xor_expression space '|' space orExpr:or_expression {  return createNode( cacheKey, Types.NODE_OP, Types.OP_OR, xorExpr, orExpr ) }
 / xor_expression
 
 xor_expression
-= andExpr:and_expression space '^' space xorExpr:xor_expression {  return createNode( cacheKey, NODE_OP, OP_XOR, andExpr, xorExpr ) }
+= andExpr:and_expression space '^' space xorExpr:xor_expression {  return createNode( cacheKey, Types.NODE_OP, Types.OP_XOR, andExpr, xorExpr ) }
 / and_expression
 
 and_expression
-= eqExpr:equality_exp space '&' space andExpr:and_expression {  return createNode( cacheKey, NODE_OP, OP_AND, eqExpr, andExpr) }
+= eqExpr:equality_exp space '&' space andExpr:and_expression {  return createNode( cacheKey, Types.NODE_OP, Types.OP_AND, eqExpr, andExpr) }
 / equality_exp
 
 equality_exp
-= relExpr:relational_exp space '==' space eqExpr:equality_exp {  return createNode( cacheKey, NODE_OP, OP_EQ, relExpr, eqExpr) }
-/ relExpr:relational_exp space '!=' space eqExpr:equality_exp {  return createNode( cacheKey, NODE_OP, OP_UEQ, relExpr, eqExpr) }
+= relExpr:relational_exp space '==' space eqExpr:equality_exp {  return createNode( cacheKey, Types.NODE_OP, Types.OP_EQ, relExpr, eqExpr) }
+/ relExpr:relational_exp space '!=' space eqExpr:equality_exp {  return createNode( cacheKey, Types.NODE_OP, Types.OP_UEQ, relExpr, eqExpr) }
 / relational_exp
 
 relational_exp
-= addExpr:add_expression space '<' space relExpr:relational_exp {  return createNode( cacheKey, NODE_OP, OP_LT, addExpr, relExpr) }
-/ addExpr:add_expression space '>' space relExpr:relational_exp {  return createNode( cacheKey, NODE_OP, OP_GT, addExpr, relExpr) }
-/ addExpr:add_expression space '<=' space relExpr:relational_exp {  return createNode( cacheKey, NODE_OP, OP_LTE, addExpr, relExpr) }
-/ addExpr:add_expression space '>=' space relExpr:relational_exp  {  return createNode( cacheKey, NODE_OP, OP_GTE, addExpr, relExpr) }
+= addExpr:add_expression space '<' space relExpr:relational_exp {  return createNode( cacheKey, Types.NODE_OP, Types.OP_LT, addExpr, relExpr) }
+/ addExpr:add_expression space '>' space relExpr:relational_exp {  return createNode( cacheKey, Types.NODE_OP, Types.OP_GT, addExpr, relExpr) }
+/ addExpr:add_expression space '<=' space relExpr:relational_exp {  return createNode( cacheKey, Types.NODE_OP, Types.OP_LTE, addExpr, relExpr) }
+/ addExpr:add_expression space '>=' space relExpr:relational_exp  {  return createNode( cacheKey, Types.NODE_OP, Types.OP_GTE, addExpr, relExpr) }
 / add_expression
 
 add_expression
-= notExpr:not_expression space '+' space addExpr:add_expression {  return createNode( cacheKey, NODE_OP, OP_ADD, notExpr, addExpr) }
-/ notExpr:not_expression space '-' space addExpr:add_expression {  return createNode( cacheKey, NODE_OP, OP_SUB, notExpr, addExpr ) }
+= notExpr:not_expression space '+' space addExpr:add_expression {  return createNode( cacheKey, Types.NODE_OP, Types.OP_ADD, notExpr, addExpr) }
+/ notExpr:not_expression space '-' space addExpr:add_expression {  return createNode( cacheKey, Types.NODE_OP, Types.OP_SUB, notExpr, addExpr ) }
 / not_expression
 
 not_expression
-= notExpr:not_op space mulExpr:mul_expression { return createNode( cacheKey, NODE_OP, notExpr, mulExpr)}
+= notExpr:not_op space mulExpr:mul_expression { return createNode( cacheKey, Types.NODE_OP, notExpr, mulExpr)}
 / mul_expression
 
 mul_expression
-= unExpr:unary_expression space '*'  space mulExpr:mul_expression  {  return createNode( cacheKey, NODE_OP, OP_MUL, unExpr, mulExpr) }
-/ unExpr:unary_expression space '/' space mulExpr:mul_expression {  return createNode( cacheKey, NODE_OP, OP_DIV, unExpr, mulExpr) }
-/ unExpr:unary_expression space '%' space mulExpr:mul_expression  {  return createNode( cacheKey, NODE_OP, OP_MOD, unExpr, mulExpr) }
+= unExpr:unary_expression space '*'  space mulExpr:mul_expression  {  return createNode( cacheKey, Types.NODE_OP, Types.OP_MUL, unExpr, mulExpr) }
+/ unExpr:unary_expression space '/' space mulExpr:mul_expression {  return createNode( cacheKey, Types.NODE_OP, Types.OP_DIV, unExpr, mulExpr) }
+/ unExpr:unary_expression space '%' space mulExpr:mul_expression  {  return createNode( cacheKey, Types.NODE_OP, Types.OP_MOD, unExpr, mulExpr) }
 / unary_expression
 
 unary_expression
-= unOP:neg_op primExpr:primitive_expression {  return createNode( cacheKey, NODE_OP, unOP, primExpr ) }
+= unOP:neg_op primExpr:primitive_expression {  return createNode( cacheKey, Types.NODE_OP, unOP, primExpr ) }
 / primExpr:primitive_expression
 
 primitive_expression
-= num:[0-9]+ {  return createNode( cacheKey, NODE_CONST, num.join(""), createNode( cacheKey, NODE_TYPE, "number" ) ) }
-/ letter:([\'][^\'][\']) { return createNode( cacheKey, NODE_CONST, letter[1], createNode( cacheKey, NODE_TYPE, "letter" ) ) }
+= num:[0-9]+ {  return createNode( cacheKey, Types.NODE_CONST, num.join(""), createNode( cacheKey, Types.NODE_TYPE, Types.TYPE_NUMBER ) ) }
+/ letter:([\'][^\'][\']) { return createNode( cacheKey, Types.NODE_CONST, letter[1], createNode( cacheKey, Types.NODE_TYPE, Types.TYPE_LETTER ) ) }
 / fun:function {return fun}
 / arg:argument {return arg}
 / str:string {return str}
@@ -313,35 +279,35 @@ Legal types used in declarations.
 */
 
 typeName
-= type:'letter' { return createNode( cacheKey, NODE_TYPE, type ); }
-/ type:'number' { return createNode( cacheKey, NODE_TYPE, type ); }
-/ type:'sentence' { return createNode( cacheKey, NODE_TYPE, type ); }
- 
+= 'letter' { return createNode( cacheKey, Types.NODE_TYPE, Types.TYPE_LETTER ); }
+/ 'number' { return createNode( cacheKey, Types.NODE_TYPE, Types.TYPE_NUMBER ); }
+/ 'sentence' { return createNode( cacheKey, Types.NODE_TYPE, Types.TYPE_SENTENCE ); }
+
 spiderType
-= type:'spider' { return createNode( cacheKey, NODE_TYPE, type ); }
+= 'spider' { return createNode( cacheKey, Types.NODE_TYPE, Types.TYPE_ARRAY ); }
 
 id
-= identifierFirst:[A-Za-z] identifierRest:[0-9A-Za-z_]* { return createNode( cacheKey, NODE_VAR, identifierFirst + identifierRest.join("") ) }
+= identifierFirst:[A-Za-z] identifierRest:[0-9A-Za-z_]* { return createNode( cacheKey, Types.NODE_VAR, identifierFirst + identifierRest.join("") ) }
 
 /*
 Arguments are identifiers and array's pieces.
 */
 
 argument
-= identifier:id "\'s" space expr:expression space 'piece' { return createNode( cacheKey, NODE_VAR_ARRAY, "array_element", identifier, expr ) }
+= identifier:id "\'s" space expr:expression space 'piece' { return createNode( cacheKey, Types.NODE_VAR_ARRAY_ELEM, "element", identifier, expr ) }
 / id
 
 string
-= ([\"] str:[^\"]* [\"]) { return createNode( cacheKey, NODE_CONST, str.join(""), createNode( cacheKey, NODE_TYPE, "sentence" ) ) }
+= ([\"] str:[^\"]* [\"]) { return createNode( cacheKey, Types.NODE_CONST, str.join(""), createNode( cacheKey, Types.NODE_TYPE, "sentence" ) ) }
 
 condition
 = '(' space expr:expression space ')' { return expr }
 
 binary_operators
-= funcName:'became ' { return funcName }
+= funcName:'became ' { return 'became' }
 
 type_operator
-= funcName:'was a ' { return funcName }
+= funcName:'was a ' { return 'was a' }
 
 unary_operators
 = funcName:'drank' { return funcName }
@@ -356,6 +322,12 @@ function_output
 
 comment_function
 = funcName:'thought' { return funcName }
+
+not_op
+= '~' { return Types.OP_NOT }
+
+neg_op
+= '-' { return Types.OP_NEG }
 
 ifFun
 = "either"
@@ -386,9 +358,3 @@ separator
 / ([ ]'and'[ ]) { return }
 / ([ ]'too'[ ]) { return }
 / ([ ]'but'[ ]) { return }
-
-not_op
-= '~' { return OP_NOT }
-
-neg_op
-= '-' { return OP_NEG }
